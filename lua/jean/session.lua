aaf95661187aae246be25d65367495ed27895df5
Daniel Leong <me@dhleong.net>
2025-06-16 19:12:06 +0000

    Attempt to improve the robustness of root dir extraction

--- lua/jean/session.lua
+++ lua/jean/session.lua
@@ -249,8 +249,21 @@ function M.from_buffer(bufnr)
     -- TODO: Other strategies?
     return M.global()
   end
+
+  local fname = vim.api.nvim_buf_get_name(nr)
   for _, client in ipairs(clients) do
-    if client.root_dir then
+    local workspace_folders = client.workspace_folders
+    if workspace_folders then
+      for _, schema in ipairs(workspace_folders) do
+        -- TODO: Do we need `vim.loop.fs_realpath`?
+        local root_dir = schema.name
+        if root_dir and vim.startswith(fname, root_dir) then
+          return M._get_or_create(root_dir, root_dir)
+        end
+      end
+    end
+
+    if client.root_dir and vim.startswith(fname, client.root_dir) then
       return M._get_or_create(client.root_dir, client.root_dir)
     end
   end
